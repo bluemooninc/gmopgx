8849592e6758c9018e11b773d62cec9bf81b4d45
Yoshi Sakai <yoshi.sakai@gmail.com>
2013-01-03 19:10:29 +0900

    Add service getRegisteredCardList

--- html/modules/gmopgx/service/Service.class.php
+++ html/modules/gmopgx/service/Service.class.php
@@ -1,5 +1,9 @@
 <?php
 if (!defined('XOOPS_ROOT_PATH')) exit();
+$comdir = XOOPS_ROOT_PATH . "/modules/gmopgx/vendor/gpay_client/src/";
+set_include_path($comdir);
+require_once('com/gmo_pg/client/input/SearchCardInput.php');
+require_once('com/gmo_pg/client/tran/SearchCard.php');
 
 class Gmopgx_Service extends XCube_Service
 {
@@ -10,6 +14,7 @@ class Gmopgx_Service extends XCube_Service
     public function prepare()
     {
         $this->addFunction(S_PUBLIC_FUNC('int checkOrderStatus(int orderId)'));
+	    $this->addFunction(S_PUBLIC_FUNC('array getRegisteredCardList()'));
     }
 
     public function checkOrderStatus()
@@ -27,4 +32,66 @@ class Gmopgx_Service extends XCube_Service
         }
         return $ret;
     }
+
+	public function &getRegisteredCardList()
+	{
+		$root = XCube_Root::getSingleton();
+		$MemberId = $root->mContext->mXoopsUser->get('uid');
+		$root->mController->setupModuleContext('gmopgx');
+		$myModuleConfig = $root->mContext->mModuleConfig;
+
+		//入力パラメータクラスをインスタンス化します
+		$input = new SearchCardInput();
+
+		//サイトID・パスワード
+		$input->setSiteId($myModuleConfig['PGCARD_SITE_ID']);
+		$input->setSitePass($myModuleConfig['PGCARD_SITE_PASS']);
+
+		//会員IDは必須です
+		$input->setMemberId($MemberId);
+
+		//カード登録連番が指定された場合、パラメータに設定します。
+		if (isset($_POST['CardSeq'])){
+			$cardSeq = xoops_getrequest('CardSeq');
+			$seqMode = xoops_getrequest('SeqMode');
+			if (0 < strlen($cardSeq)) {
+				//登録カード連番
+				$input->setCardSeq($cardSeq);
+				$input->setSeqMode($seqMode);
+			}
+		}
+		//API通信クラスをインスタンス化します
+		$exe = new SearchCard();
+
+		//パラメータオブジェクトを引数に、実行メソッドを呼びます。
+		//正常に終了した場合、結果オブジェクトが返るはずです。
+		$output = $exe->exec($input);
+
+		//実行後、その結果を確認します。
+		if ($exe->isExceptionOccured()) {
+			//取引の処理そのものがうまくいかない（通信エラー等）場合、例外が発生します。
+			$this->error_message = "Connection ERROR!";
+			return NULL;
+		} else {
+			//例外が発生していない場合、出力パラメータオブジェクトが戻ります。
+			if ($output->isErrorOccurred()) { //出力パラメータにエラーコードが含まれていないか、チェックしています。
+				$errorHandle = new ErrorHandler();
+				$errorList = $output->getErrList();
+				$this->error_message = "";
+				foreach ($errorList as $errorInfo) {
+					/* @var $errorInfo ErrHolder */
+					$this->error_message .= '<li>'
+						. $errorInfo->getErrCode()
+						. ':' . $errorInfo->getErrInfo()
+						. ':' . $errorHandle->getMessage($errorInfo->getErrInfo())
+						. '</li>';
+				}
+				return NULL;
+			}
+			//例外発生せず、エラーの戻りもないので、正常とみなします。
+			//このif文を抜けて、結果を表示します。
+		}
+		$cardList = $output->getCardList();
+		return $cardList;
+	}
 }
